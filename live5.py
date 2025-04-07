ea09d8a4ad5ba5081d6ddaf4987d053650169ff9
vidhanti-surya <siyayadav8080@gamail.com>
2025-04-07 10:50:30 +0530

    MY first Commit

---
+++ live5.py
@@ -0,0 +1,19 @@
+#def surya():
+  #  print("hey print the hello world ")
+
+
+
+#def sss(n):
+   # print("welcome surya how are u", name )
+
+
+
+
+#function to add two number
+def add1(no1,no2):
+    res = no1+no2
+    print(res)
+
+
+add1(3457,564)
+
